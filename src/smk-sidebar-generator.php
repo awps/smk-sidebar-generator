2639b917742f5c83b33e3bc73d7769352495b336
Andrei Surdu <smartik89@gmail.com>
2026-02-17 09:57:38 +0200

    Update tested version to 6.9.1

--- src/smk-sidebar-generator.php
+++ src/smk-sidebar-generator.php
@@ -10,7 +10,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Requires PHP: 5.3
  * Requires at least: 4.0
- * Tested up to: 6.4.1
+ * Tested up to: 6.9.1
  * Text Domain: smk-sidebar-generator
  */
 
